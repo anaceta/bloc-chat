09eeba91d9c851bf062379313a9198dcecbfe88e
Theodore Kim <taehyunk@buffalo.edu>
2018-02-22 03:22:54 -0500

    Bloc Chat completed, no css though

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,9 @@
 (function() {
-    function HomeCtrl(Room, Message, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal, $cookies) {
         this.rooms = Room.all;
-        this.currentRoom = null
+        this.currentRoom = null;
+        this.currentUser = $cookies.get('blocChatCurrentUser');
+        // this.message = "";
         this.addRoom = function() {
             $uibModal.open({
                 templateUrl: '/templates/modal.html',
@@ -15,9 +17,25 @@
           this.messages = Message.getByRoomId(this.currentRoom.$id);
         }
 
+        this.sendMessage = function (message) {
+          this.newMessage.roomId = this.currentRoom.$id;
+          this.newMessage.username = this.currentUser;
+          Message.send(this.newMessage);
+
+        //  var newMessage = {
+          //  content: message,
+            //roomId: this.currentRoom.$id,
+            // Could potentially just set roomId here to whatever id is needed
+            //
+            //username: this.currentUser
+          //}
+
+
+        }
+
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', '$cookies', HomeCtrl]);
 })();
